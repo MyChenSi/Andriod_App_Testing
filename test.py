569ad10bddfbe350cc1484531284b8aa655f5d4b
sich <chen.si@hp.com>
2020-07-17 22:53:12 +0800

    Search test ready

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
  
 caps = {}
 caps["platformName"] = "Android"
@@ -10,11 +11,15 @@ caps["resetKeyboard"] = True
 caps["unicodeKeyboard"] = True
  
 driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
- 
+
 el1 = driver.find_element_by_id("android:id/button1")
 el1.click()
-el2 = driver.find_element_by_id("0b9c3d34-e4fc-4205-b8af-c2b788e197db")
+el2 = driver.find_element_by_id("com.baidu.searchbox:id/close_btn")
 el2.click()
-
- 
+el5 = driver.find_element_by_id("com.baidu.searchbox:id/baidu_searchbox")
+el5.click()
+el8 = driver.find_element_by_id("com.baidu.searchbox:id/SearchTextInput")
+el8.send_keys("test")
+el9 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.TextView")
+el9.click()
 driver.quit()
